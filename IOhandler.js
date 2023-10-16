cbd3a2d3cdeb618602c3c3dbc6851bd2d18360d2
Riona Wong <Riona.Wong1128@gmail.com>
2023-10-16 13:30:30 -0700

    forgot to git push each function separately...

--- IOhandler.js
+++ IOhandler.js
@@ -8,12 +8,14 @@
  *
  */
 
+const { dirname } = require("path");
 const { pipeline } = require("stream");
 
 const unzipper = require("unzipper"),
   fs = require("fs"),
   PNG = require("pngjs").PNG,
   path = require("path");
+const AdmZip = require('adm-zip');
 
 /**
  * Description: decompress file from given pathIn, write to given pathOut
@@ -24,14 +26,19 @@ const unzipper = require("unzipper"),
  */
 const unzip = (pathIn, pathOut) => {
 
-  return new Promise ((resolve, reject)=>{
-    fs.createReadStream(pathIn)
-    .on("error",(err)=> reject(err))
-    .pipe(unzipper.Extract({ path: pathOut }))
-    .on("error",(err)=> reject(err))
-    .on("end", (err) => resolve(err))
-})
-};
+  return new Promise((resolve, reject) => {
+    const zip = new AdmZip(pathIn);
+    try {
+      zip.extractAllTo(pathOut, /*overwrite*/ true);
+      console.log('Extraction completed successfully!');
+      resolve();
+    } catch (error) {
+      console.error('Extraction error:', error);
+      reject(error);
+    }
+  });
+}
+
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -39,20 +46,94 @@ const unzip = (pathIn, pathOut) => {
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
 
+const readDir = (dir) => {
+  return new Promise((resolve, reject)=>{
+    listOfPaths = []
+    fs.readdir(dir,(err, file)=>{
+      console.log("reading data from path")
+      if (err)
+        reject(err)
+      else{
+        for(x in file){
+          listOfPaths.push(path.join(dir,file[x].toString()))
+        }
+        const filteredFiles = listOfPaths.filter(file => file.endsWith('.png'));
+        resolve(filteredFiles)
+        console.log(filteredFiles)
+      }
+    })
+  })
+
+};
+
+  
 /**
  * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
  *
- * @param {string} filePath
- * @param {string} pathProcessed
- * @return {promise}
- */
-const grayScale = (pathIn, pathOut) => {};
+//  * @param {string} filePath
+//  * @param {string} pathProcessed
+//  * @return {promise}
+//  */
+
+const processImage = (file,pathOut) =>{
+        const fileName = path.basename(file);
+        const outputPath = path.join(pathOut, `${fileName}_grayscale.png`);
+        return new Promise((resolve, reject) => {
+        const inputStream = fs.createReadStream(file);
+        const png = new PNG();
+
+        inputStream
+            .pipe(png)
+            .on('parsed', function() {
+                for (let y = 0; y < this.height; y++) {
+                    for (let x = 0; x < this.width; x++) {
+                        const idx = (this.width * y + x) << 2;
+                        const avg = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+                        this.data[idx] = avg;
+                        this.data[idx + 1] = avg;
+                        this.data[idx + 2] = avg;
+                    }
+                }
+
+                const outputStream = fs.createWriteStream(outputPath);
+                this.pack().pipe(outputStream);
+
+                outputStream.on('finish', () => {
+                    console.log(`Image converted to grayscale and saved at ${outputPath}`);
+                    resolve();
+                });
+            })
+            .on('error', error => {
+                console.error(`Error processing ${file}:`, error);
+                reject(error);
+            });
+            
+    });
+}
+
+
+const grayScale = (pathIn, pathOut) => {
+  if (!fs.existsSync(pathOut)) {
+    fs.mkdirSync(pathOut, { recursive: true });
+}
+  readDir(pathIn)
+    .then(array => {
+      for (file in array){
+        processImage(array[file],pathOut)
+  } 
+    })
+    .catch(error => {
+        console.error('Error :', error);
+    });
+
+}
 
 module.exports = {
   unzip,
   readDir,
   grayScale,
 };
+
+readDir("/Users/riona/Documents/ACIT2520 Web Development/week6/Lab6")
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const { pipeline } = require("stream");
+
 /*
  * Project: Milestone 1
  * File Name: main.js
@@ -13,3 +15,10 @@ const IOhandler = require("./IOhandler");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
+const {unzip, grayScale} = require("./IOhandler.js");
+
+
+unzip(zipFilePath, pathUnzipped),
+grayScale(pathUnzipped,pathProcessed)
+
+
